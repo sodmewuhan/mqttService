36554d434bb0d08765cf5a5475c15811012203b3
sodmewuhan <sodmewuhan@126.com>
2018-06-02 17:31:11 +0800

    提交pom.xml

---
+++ src/main/java/com/datasensorn/mqttservice/model/biz/User.java
@@ -0,0 +1,20 @@
+package com.datasensorn.mqttservice.model.biz;
+
+/**
+ * 用户对象
+ */
+public class User {
+
+    private Long id;
+
+    //用户姓名
+    private String name;
+
+    //用户密码
+    private String password;
+
+    //用户电话
+    private String phoneNum;
+
+    //用户鱼塘
+}
