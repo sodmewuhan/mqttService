0c8a7562f9d27d6d41b2ba1299be58320e06c9f2
cailong <issuser@ISS460002000291.isoftstone.com>
2018-06-25 18:43:00 +0800

    增加服务层功能

---
+++ src/main/java/com/datasensorn/mqttservice/controller/DispatchInstruction.java
@@ -0,0 +1,23 @@
+package com.datasensorn.mqttservice.controller;
+
+import com.datasensorn.mqttservice.handle.SendDataHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 向盒子下发指令
+ */
+@RestController
+@RequestMapping("/app/dispatch")
+public class DispatchInstruction {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(DispatchInstruction.class);
+
+    @Autowired
+    SendDataHandler sendDataHandler;//下发指令消息服务
+
+
+}
