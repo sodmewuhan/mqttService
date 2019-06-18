18add3bd693ccebb1fb22588f1bef2cc12510b10
sodmewuhan <1479qwert@>
2019-06-18 15:46:41 +0800

    数据库加密

--- src/main/java/com/datasensorn/mqttservice/Application.java
+++ src/main/java/com/datasensorn/mqttservice/Application.java
@@ -1,16 +1,14 @@
 package com.datasensorn.mqttservice;
 
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
--- src/main/java/com/datasensorn/mqttservice/controller/page/UploadFileRestControll.java
+++ src/main/java/com/datasensorn/mqttservice/controller/page/UploadFileRestControll.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Deprecated
 @RequestMapping(value="/page/appVersion")
 public class UploadFileRestControll {
 
